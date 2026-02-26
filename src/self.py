c163759737fa31b9f97db9d4de243331fee9409d
saeed mhd <saeedmrpg@gmail.com>
2026-02-26 23:46:39 +0330

    add helth check

--- src/self.py
+++ src/self.py
@@ -4,6 +4,7 @@ import sys
 import time
 
 import aiohttp
+from aiohttp import web
 import scren_tools
 from dotenv import load_dotenv
 from mistralai import Mistral
@@ -277,3 +278,11 @@ async def main():
     await client.run_until_disconnected()
 
 asyncio.run(main())
+
+async def handle(request):
+    return web.Response(text="iQ Self running!")
+
+app = web.Application()
+app.router.add_get("/", handle)
+
+web.run_app(app, port=8000)
